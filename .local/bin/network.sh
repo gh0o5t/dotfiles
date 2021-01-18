6e2ce9c32cf6ef0ee8acc5951da1d187fc293569
gh0o5t <gh0o5t@protonmail.com>
2021-01-18 19:12:23 +0100

    Network script made better

---
+++ .local/bin/network.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+ETHER=' '
+WIFI=' '
+message=""
+
+for interface in $(nmcli device status | grep 'connected '| awk '{ print $1 }'); do
+    TYPE=$(nmcli device show $interface | grep 'TYPE' | awk '{ print $2 }')
+    IP=$(nmcli device show $interface | grep 'IP4\.ADDRESS\[1\]' | awk '{ print $2 }')
+    CONNECTION=$(nmcli device show $interface | grep 'CONNECTION' | awk '{ print $2 }')
+    [ "$TYPE" = "wifi" ] && message="$message $WIFI $IP ($CONNECTION) "
+    [ "$TYPE" = "ethernet" ] && message="$message $ETHER $IP " 
+done
+
+printf "$message"
+
