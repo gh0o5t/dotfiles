d2987e88150f4015f10362e2dc91c15ca26a7e29
gh0o5t <gh0o5t@protonmail.com>
2021-01-19 17:07:39 +0100

    Fixed error

--- .local/bin/network.sh
+++ .local/bin/network.sh
@@ -4,7 +4,7 @@ ETHER=' '
 WIFI=' '
 message=""
 
-for interface in $(nmcli device status | grep 'connected '| awk '{ print $1 }'); do
+for interface in $(nmcli device status | grep ' connected '| awk '{ print $1 }'); do
     TYPE=$(nmcli device show $interface | grep 'TYPE' | awk '{ print $2 }')
     IP=$(nmcli device show $interface | grep 'IP4\.ADDRESS\[1\]' | awk '{ print $2 }')
     CONNECTION=$(nmcli device show $interface | grep 'CONNECTION' | awk '{ print $2 }')
